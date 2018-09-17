1da847faa17edf3a607c9095060267e6d3662f32
web-zy <527918571@qq.com>
2018-09-17 16:02:47 +0800

    提交

--- projects/src/components/Index/News/Order/utrl/orderFun.js
+++
@@ -1,9 +0,0 @@
-function setUserData(data){
-    let obj = {};
-    obj.head_image_id = data.head_image_id
-    obj.user_name = data.app_user_name
-    return obj
-}
-export default{
-    setUserData:setUserData
-}
--- projects/src/index.js
+++
@@ -1,8 +0,0 @@
-import utrl from "./controller/utrl.js"
-
-if(localStorage.pass){
-	let res = utrl.signIn({
-        uid:localStorage.user,
-        password:localStorage.pass
-    })
-}
